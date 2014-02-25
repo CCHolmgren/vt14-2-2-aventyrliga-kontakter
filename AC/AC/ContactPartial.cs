00ca3864ef747ebb99d213b0e506f847d330f652
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-02-25 12:19:04 +0100

    Trying with Data annotations

---
+++ AC/AC/ContactPartial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.ComponentModel.DataAnnotations;
+
+namespace AC
+{
+    [MetadataType(typeof(ContactMetadata))]
+    public partial class ContactPartial
+    {
+    }
+    public class ContactMetadata
+    {
+    }
+}
